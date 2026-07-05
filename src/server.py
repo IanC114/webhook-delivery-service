6c31a19d2a4eb59ff9d0e04f0a27af188820449c
Ian <epicgolfer@outlook.com>
2026-07-04 18:44:34 -0700

    Created fastapi server to listen and forward endpoints, added docker, and added nginx

---
+++ src/server.py
@@ -0,0 +1,62 @@
+import logging
+from collections import defaultdict
+from json import JSONDecodeError
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Response, Request
+import uvicorn
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.websockets = defaultdict(list)
+
+    async def connect(self, hook_id, websocket: WebSocket):
+        await websocket.accept()
+        self.websockets[hook_id].append(websocket)
+
+    def disconnect(self, hook_id, websocket: WebSocket):
+        self.websockets[hook_id].remove(websocket)
+
+        if not self.websockets[hook_id]:
+            del self.websockets[hook_id]
+
+    async def broadcast(self, hook_id, data):
+        for websocket in self.websockets.get(hook_id, []):
+            await websocket.send_json(data)
+
+
+app = FastAPI()
+manager = ConnectionManager()
+
+
+@app.post("/webhook/")
+@app.post("/webhook/{hook_id}")
+async def post_webhook(request: Request, response: Response, hook_id: str | None = None):
+    try:
+        if hook_id:
+            datajson = await request.json()
+            response.status_code = 200
+            await manager.broadcast(hook_id, datajson)
+        else:
+            response.status_code = 404
+    except (JSONDecodeError, TypeError):
+        logging.exception("Invalid JSON!")
+        response.status_code = 400
+    finally:
+        return {"status_code": response.status_code}
+
+
+@app.websocket("/tunnel/{hook_id}")
+async def websocket_endpoint(hook_id: str, websocket: WebSocket):
+    await manager.connect(hook_id, websocket)
+
+    # keep connection alive to detect disconnections
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(hook_id, websocket)
+
+
+if __name__ == "__main__":
+    uvicorn.run("src.server:app", host="localhost", port=5000, reload=True)
